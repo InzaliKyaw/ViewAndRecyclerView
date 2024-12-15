6eea4eceb5700a1eee150d6745816274401893ca
Zoey <inzalikyaw@gmail.com>
2024-12-15 20:25:53 +1100

    first set up

--- app/src/main/java/com/example/viewandroid/MainActivity.kt
+++ app/src/main/java/com/example/viewandroid/MainActivity.kt
@@ -15,32 +15,7 @@ import com.example.viewandroid.ui.theme.ViewAndroidTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ViewAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ViewAndroidTheme {
-        Greeting("Android")
-    }
-}
