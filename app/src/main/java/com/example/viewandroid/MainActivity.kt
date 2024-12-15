b99ed9167761b196067f9311808585ce264d654a
Zoey <inzalikyaw@gmail.com>
2024-12-16 01:02:23 +1100

    set up

--- app/src/main/java/com/example/viewandroid/MainActivity.kt
+++ app/src/main/java/com/example/viewandroid/MainActivity.kt
@@ -1,16 +1,10 @@
 package com.example.viewandroid
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.viewandroid.ui.theme.ViewAndroidTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
