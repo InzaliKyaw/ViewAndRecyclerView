87df433916cd5266bec91dc08a90cb7cf23b512c
Zoey <inzalikyaw@gmail.com>
2024-12-15 23:55:33 +1100

    data binding

--- app/src/main/java/com/example/viewandroid/Constant.kt
+++ app/src/main/java/com/example/viewandroid/Constant.kt
@@ -1,4 +1,3 @@
 package com.example.viewandroid
 
-class Constant {
-}
+const val NAME_DATA = ""
--- app/src/main/java/com/example/viewandroid/MainActivity.kt
+++ app/src/main/java/com/example/viewandroid/MainActivity.kt
@@ -1,21 +1,67 @@
 package com.example.viewandroid
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.viewandroid.ui.theme.ViewAndroidTheme
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.viewandroid.databinding.ActivityMainBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    var userInput:String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        // Check if there's a saved state
+
+        if (savedInstanceState != null) {
+            // Retrieve data from the Bundle
+            val userInput = savedInstanceState.getString("USER_INPUT", "")
+            val displayText = savedInstanceState.getString("DISPLAY_TEXT", "")
+
+            // Restore data to views
+            binding.txtUserInput.setText(userInput)
+            binding.txtDisplay.text = displayText
+        }
+
+
+        binding.btnDone.setOnClickListener {
+            userInput   = binding.txtUserInput.text.toString()
+            // Validate the input
+            if (validateInput(userInput.toString())) {
+                binding.txtDisplay.text = userInput
+            } else {
+                Toast.makeText(this,"Please fill valid value",Toast.LENGTH_LONG).show()
+            }
+        }
     }
+
+
+    // Function to validate the user input
+    private fun validateInput(input: String): Boolean {
+        return !TextUtils.isEmpty(input.trim())
+    }
+
+
+    // onStart()
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedString = savedInstanceState.getString("USER_INPUT") // Retrieve the String
+        val savedCounter = savedInstanceState.getInt("DISPLAY_TEXT") // Retrieve the Int
+
+        // Use the retrieved values as needed
+        Log.d("TAG", "Retrieved String: $savedString, Counter: $savedCounter")
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("USER_INPUT", binding.txtUserInput.text.toString())
+        outState.putString("DISPLAY_TEXT", binding.txtDisplay.text.toString())
+    }
+
+
 }
 
